76b9bf1bbd07a734c13c2b4a8c2ee9c0192023b4
Syed Ali Fathima A <syedalifathima.a@publicissapient.com>
2022-11-08 18:58:49 +0530

    first commit

---
+++ src/main/java/com/example/jenkinsdockerone/JenkinsDockerOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsdockerone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsdockerone/JenkinsDockerOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsdockerone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
