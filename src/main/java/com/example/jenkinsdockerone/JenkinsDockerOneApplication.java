4deaf3929452942283cb8bc4a7d9beb77d9fc0e8
Syed Ali Fathima A <syedalifathima.a@publicissapient.com>
2022-12-14 01:28:05 +0530

    method added

--- src/main/java/com/example/jenkinsdockerone/JenkinsDockerOneApplication.java
+++ src/main/java/com/example/jenkinsdockerone/JenkinsDockerOneApplication.java
@@ -1,13 +1,24 @@
 package com.example.jenkinsdockerone;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsDockerOneApplication {
 
+	public static void getMessage(String name) {
+
+		System.out.println("Hi " + name);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Test one !!");
+
+		try (Scanner sc = new Scanner(System.in)) {
+			String n = sc.next("Enter name, ");
+			getMessage(n);
+		}
 		SpringApplication.run(JenkinsDockerOneApplication.class, args);
 	}
 
